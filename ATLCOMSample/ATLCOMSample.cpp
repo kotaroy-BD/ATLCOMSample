72c8aa678e4b93aa637a3108422be0e1c60ef603
phoge <kotaroyoshida@hotmail.com>
2024-01-11 21:26:36 +0900

    Initial commit. Code is automatically generated by VS.

---
+++ ATLCOMSample/ATLCOMSample.cpp
@@ -0,0 +1,30 @@
+﻿// ATLCOMSample.cpp : WinMain の実装
+
+
+#include "pch.h"
+#include "framework.h"
+#include "resource.h"
+#include "ATLCOMSample_i.h"
+
+
+using namespace ATL;
+
+
+class CATLCOMSampleModule : public ATL::CAtlExeModuleT< CATLCOMSampleModule >
+{
+public :
+	DECLARE_LIBID(LIBID_ATLCOMSampleLib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_ATLCOMSAMPLE, "{77b9de87-2fee-4160-a014-91a2caeec839}")
+};
+
+CATLCOMSampleModule _AtlModule;
+
+
+
+//
+extern "C" int WINAPI _tWinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/,
+								LPTSTR /*lpCmdLine*/, int nShowCmd)
+{
+	return _AtlModule.WinMain(nShowCmd);
+}
+
